eb4e70cc11d9af893b1f7e9af524264243707b8f
Ivan Semikin <i.semikin.94@icloud.com>
2025-07-07 20:31:42 +0200

    Initial Commit

---
+++ ArtistsGallery/ArtistsGalleryApp.swift
@@ -0,0 +1,17 @@
+//
+//  ArtistsGalleryApp.swift
+//  ArtistsGallery
+//
+//  Created by Иван Семикин on 07/07/2025.
+//
+
+import SwiftUI
+
+@main
+struct ArtistsGalleryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
